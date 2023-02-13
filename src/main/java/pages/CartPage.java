a1ed2a6a6d69ae00211987ac8601e1261e210164
Aykut ABUHAN <abuhanaykut@yahoo.com>
2023-02-13 23:03:30 +0300

    Added new methods and locators

--- src/main/java/pages/CartPage.java
+++ src/main/java/pages/CartPage.java
@@ -6,15 +6,21 @@ import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 
 public class CartPage extends BasePage{
-    @FindBy (css = "div[class*=basket] h1")
+    @FindBy(css = "div[class*=basket] h1")
     private WebElement myBasketLocator;
-
-    public CartPage(WebDriver driver) {
-        super(driver);
-    }
+    @FindBy(css = "div[data-test-id='account-new']")
+    private WebElement myAccountLocator;
+    @FindBy(xpath = "//a[contains(text(),'Giriş yap')]")
+    private WebElement enterMyAccountLocator;
 
     public boolean isOnBasketPage(){
         waitUntil(ExpectedConditions.visibilityOf(myBasketLocator), 5);
         return isDisplayed(myBasketLocator);
+    }
+    public void clickMyAccount(){
+        m_WaitPage.until(ExpectedConditions.elementToBeClickable(myAccountLocator)).click();
+    }
+    public void clickEnterMyAccount(){
+        m_WaitPage.until(ExpectedConditions.elementToBeClickable(enterMyAccountLocator)).click();
     }
 }
