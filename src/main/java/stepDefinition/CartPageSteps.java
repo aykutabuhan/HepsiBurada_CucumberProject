d0b692dfa4b653c96d66f65a75d3f3fc2fe8e34a
Aykut ABUHAN <abuhanaykut@yahoo.com>
2023-02-13 23:04:55 +0300

    Designed reference and added new method

--- src/main/java/stepDefinition/CartPageSteps.java
+++ src/main/java/stepDefinition/CartPageSteps.java
@@ -1,15 +1,18 @@
 package stepDefinition;
 
-import drivers.Driver;
 import io.cucumber.java.en.And;
-import io.cucumber.java.en.Given;
-import org.openqa.selenium.WebDriver;
-import util.PropertyUtil;
+import pages.CartPage;
 
 public class CartPageSteps {
 
-    @And("click to complete the shopping button")
-    public void clickToCompleteTheShoppingButton() {
-        // Bunla ilgili method yazılacak(Locator bulunacak)
+    private static final CartPage m_cartPage = new CartPage();
+    @And("click to my account")
+    public void clickToAccount() {
+        m_cartPage.clickMyAccount();
+    }
+
+    @And("click to login")
+    public void clickToLogin() {
+        m_cartPage.clickEnterMyAccount();
     }
 }
